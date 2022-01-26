b6adec90251f1c37213418bddead1411a0380456
Shubha Changappa Palachanda <shubha8196@gmail.com>
2022-01-25 20:26:50 -0800

    Revert "Added first oteration of entropy calculation"
    This reverts commit 3cbec2c80660e3656d357e28ecc502b9981a6d9d.

--- calc_entropy.py
+++ calc_entropy.py
@@ -1,10 +1,2 @@
-import numpy as np
-
-
-def entropy(probs):
-    """Assumes that input is a numpy array and that it is a valid prob vector."""
-    entropy_value= -(probs * np.log(probs))
-    return entropy_value
-
 if __name__ == "__main__":
     pass
